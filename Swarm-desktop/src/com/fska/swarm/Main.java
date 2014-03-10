621cf30126770fa29b1f25f4566a8c6e4663af19
Fred Skallos <fredska@gmail.com>
2014-03-10 14:41:58 -0400

    Initial Commit

---
+++ Swarm-desktop/src/com/fska/swarm/Main.java
@@ -0,0 +1,16 @@
+package com.fska.swarm;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Swarm";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Swarm(), cfg);
+	}
+}
